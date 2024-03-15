41e8334424e74b6adc41cd78afcb5fcbc70c6df1
lokesh9783589128 <163140341+lokesh9783589128@users.noreply.github.com>
2024-03-15 18:54:53 +0530

    Add files via upload

---
+++ metaverse project/metaverse.js
@@ -0,0 +1,18 @@
+{/* <script>
+
+       function img(){
+            alert("soon")
+        }
+        function button(){
+            alert("soon!")
+        }
+
+        burger = document.querySelector('.burger')
+navlist = document.querySelector('.nav-list')
+navlistli= document.querySelector('.nav-list li')
+
+burger.addEventListener('click',()=>{
+    navlistli.classList.toggle('h-list');
+    navlist.classList.toggle('h-list');
+})
+  </script> */}
